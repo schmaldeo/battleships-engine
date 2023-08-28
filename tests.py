f4e3f85e4c33dc4fade2229a72f7161bc2dccce1
schmatteo <michal.skrzyszowski@hotmail.com>
2023-08-28 14:00:43 +0200

    Update tests

--- tests.py
+++ tests.py
@@ -2,12 +2,12 @@ import unittest
 from main import *
 
 
-class TestGame(unittest.TestCase):
+class TestSinglePlayerGame(unittest.TestCase):
     def test_put_destroyer(self):
-        game = Game()
-        game.put_ship(ShipType.DESTROYER, 0, 0, Direction.HORIZONTAL)
+        game = SinglePlayerGame()
+        game.player.put_ship(ShipType.DESTROYER, 0, 0, Direction.HORIZONTAL)
         self.assertTrue(
-            game.board ==
+            game.player.board ==
             [[Field.TAKEN, Field.TAKEN, Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY],
              [Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY],
              [Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY],
@@ -18,10 +18,10 @@ class TestGame(unittest.TestCase):
              [Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY]])
 
     def test_put_cruiser(self):
-        game = Game()
-        game.put_ship(ShipType.CRUISER, 0, 0, Direction.HORIZONTAL)
+        game = SinglePlayerGame()
+        game.player.put_ship(ShipType.CRUISER, 0, 0, Direction.HORIZONTAL)
         self.assertTrue(
-            game.board ==
+            game.player.board ==
             [[Field.TAKEN, Field.TAKEN, Field.TAKEN, Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY],
              [Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY],
              [Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY],
@@ -32,10 +32,10 @@ class TestGame(unittest.TestCase):
              [Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY]])
 
     def test_put_battleship(self):
-        game = Game()
-        game.put_ship(ShipType.BATTLESHIP, 0, 0, Direction.HORIZONTAL)
+        game = SinglePlayerGame()
+        game.player.put_ship(ShipType.BATTLESHIP, 0, 0, Direction.HORIZONTAL)
         self.assertTrue(
-            game.board ==
+            game.player.board ==
             [[Field.TAKEN, Field.TAKEN, Field.TAKEN, Field.TAKEN, Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY],
              [Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY],
              [Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY],
@@ -46,10 +46,10 @@ class TestGame(unittest.TestCase):
              [Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY]])
 
     def test_put_destroyer_vertically(self):
-        game = Game()
-        game.put_ship(ShipType.DESTROYER, 0, 0, Direction.VERTICAL)
+        game = SinglePlayerGame()
+        game.player.put_ship(ShipType.DESTROYER, 0, 0, Direction.VERTICAL)
         self.assertTrue(
-            game.board ==
+            game.player.board ==
             [[Field.TAKEN, Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY],
              [Field.TAKEN, Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY],
              [Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY],
@@ -60,19 +60,19 @@ class TestGame(unittest.TestCase):
              [Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY]])
 
     def test_custom_board_size(self):
-        game = Game(4, 4)
+        game = SinglePlayerGame(4, 4)
         self.assertTrue(
-            game.board ==
+            game.player.board ==
             [[Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY],
              [Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY],
              [Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY],
              [Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY]])
 
     def test_hit_ship(self):
-        game = Game(4, 4)
-        ship = game.put_ship(ShipType.DESTROYER, 0, 0, Direction.HORIZONTAL)
+        game = SinglePlayerGame(4, 4)
+        ship = game.player.put_ship(ShipType.DESTROYER, 0, 0, Direction.HORIZONTAL)
         self.assertTrue(
-            game.board ==
+            game.player.board ==
             [[Field.TAKEN, Field.TAKEN, Field.EMPTY, Field.EMPTY],
             [Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY],
             [Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY],
@@ -84,13 +84,13 @@ class TestGame(unittest.TestCase):
         self.assertTrue(hit_shot.ship == ship)
         self.assertTrue(hit_shot.ships_left == 1)
         self.assertTrue(
-            hit_shot.hits_misses_board == [[Field.EMPTY, Field.HIT, Field.EMPTY, Field.EMPTY],
-                                           [Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY],
-                                           [Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY],
-                                           [Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY]])
+            hit_shot.hit_miss_board == [[Field.EMPTY, Field.HIT, Field.EMPTY, Field.EMPTY],
+                                        [Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY],
+                                        [Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY],
+                                        [Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY]])
 
         self.assertTrue(
-            game.board ==
+            game.player.board ==
             [[Field.TAKEN, Field.TAKEN, Field.EMPTY, Field.EMPTY],
              [Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY],
              [Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY],
@@ -101,19 +101,19 @@ class TestGame(unittest.TestCase):
         self.assertTrue(not missed_shot.hit)
         self.assertTrue(missed_shot.ship is None)
         self.assertTrue(missed_shot.ships_left == 1)
-        self.assertTrue(missed_shot.hits_misses_board == [[Field.EMPTY, Field.HIT, Field.EMPTY, Field.EMPTY],
-                                                          [Field.MISSED, Field.EMPTY, Field.EMPTY, Field.EMPTY],
-                                                          [Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY],
-                                                          [Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY]])
+        self.assertTrue(missed_shot.hit_miss_board == [[Field.EMPTY, Field.HIT, Field.EMPTY, Field.EMPTY],
+                                                       [Field.MISSED, Field.EMPTY, Field.EMPTY, Field.EMPTY],
+                                                       [Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY],
+                                                       [Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY]])
 
     def test_destroy_ship(self):
-        game = Game(4, 4)
-        ship = game.put_ship(ShipType.DESTROYER, 0, 0, Direction.HORIZONTAL)
+        game = SinglePlayerGame(4, 4)
+        ship = game.player.put_ship(ShipType.DESTROYER, 0, 0, Direction.HORIZONTAL)
         game.shoot(0, 1)
         game.shoot(1, 0)
         shot = game.shoot(0, 0)
         self.assertTrue(
-            game.board ==
+            game.player.board ==
             [[Field.TAKEN, Field.TAKEN, Field.EMPTY, Field.EMPTY],
              [Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY],
              [Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY],
@@ -123,16 +123,16 @@ class TestGame(unittest.TestCase):
         self.assertTrue(shot.hit)
         self.assertTrue(shot.ship == ship)
         self.assertTrue(shot.ships_left == 0)
-        self.assertTrue(shot.hits_misses_board == [[Field.HIT, Field.HIT, Field.EMPTY, Field.EMPTY],
-                                                   [Field.MISSED, Field.EMPTY, Field.EMPTY, Field.EMPTY],
-                                                   [Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY],
-                                                   [Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY]])
+        self.assertTrue(shot.hit_miss_board == [[Field.HIT, Field.HIT, Field.EMPTY, Field.EMPTY],
+                                                [Field.MISSED, Field.EMPTY, Field.EMPTY, Field.EMPTY],
+                                                [Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY],
+                                                [Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY]])
 
     def test_random_spawn(self):
-        game = Game(6, 6)
-        game.random_spawn(ShipType.DESTROYER)
+        game = SinglePlayerGame(6, 6)
+        game.player.random_spawn(ShipType.DESTROYER)
         self.assertFalse(
-            game.board == [[Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY],
+            game.player.board == [[Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY],
                         [Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY],
                         [Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY],
                         [Field.EMPTY, Field.EMPTY, Field.EMPTY, Field.EMPTY]]
